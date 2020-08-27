e77f91f5ee4916dd3f90854f9f2c8029098e1092
Collin Holmquist <collinholmquist@gmail.com>
2020-08-27 09:01:44 -0500

    Create TestStatsLab.java

---
+++ TestStatsLab.java
@@ -0,0 +1,15 @@
+/*
+ Create a program that takes 5 test grades (on a scale from 0 - 100) ... sorry no Standards Based Grading
+ 
+The program should 
+ - compute the average of the 5 test scores.  
+ - compute the variance 
+ - comptue the standard deviation of the scores 
+ 
+ If we have the avg, then the variance is:  [(testScore1 - avg) ^ 2  + (testScore2 - avg) ^ 2 + ... ] / number of tests
+ The standard deviation is the square root of that value.  
+ 
+ To calculate the square root you can use:  Math.sqrt(value)
+ 
+
+*/

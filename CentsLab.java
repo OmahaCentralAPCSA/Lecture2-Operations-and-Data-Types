75d894d27dfb076a7d069bc6998c38b95c3bde9a
Collin Holmquist <collinholmquist@gmail.com>
2020-08-27 08:55:25 -0500

    Create CentsLab.java

---
+++ CentsLab.java
@@ -0,0 +1,19 @@
+/*
+Write a program that gives the exact change with the least number of coins for a given number of cents.  
+
+Example: 137 cents should output: 5 quarters, 1 dimes, 0 nickels, 2 pennies
+Example: 50 cents should output:  2 quarters, 0 dimes, 0 nickels, 0 pennies
+
+*/
+
+public class DataTypesLab {
+  
+   public static void main(String [] args) {
+   
+      int totalCents = 137 //change this number to test various values.  
+      
+      //your code goes below. 
+   
+   }
+
+}

bbf62422c50205f82ad519cf09ccd984caa7f548
Collin Holmquist <collinholmquist@gmail.com>
2020-08-27 09:05:13 -0500

    Update TestStatsLab.java

--- TestStatsLab.java
+++ TestStatsLab.java
@@ -5,11 +5,21 @@ The program should
  - compute the average of the 5 test scores.  
  - compute the variance 
  - comptue the standard deviation of the scores 
+ - print all the values out.  Use Strings to help label what each value is.  ("The standard deviation of the test scores is: ") 
  
  If we have the avg, then the variance is:  [(testScore1 - avg) ^ 2  + (testScore2 - avg) ^ 2 + ... ] / number of tests
  The standard deviation is the square root of that value.  
  
  To calculate the square root you can use:  Math.sqrt(value)
  
-
+Reminder:  you will have to declare, assign and use variables.  Remember data type is important. 
 */
+
+public class TestStatsLab {
+
+  public static void main (String[] args) {
+  
+      //code goes here 
+  }
+
+}
